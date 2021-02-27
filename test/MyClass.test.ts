f86de51e4870ca83d99fe066c2c7dd5c16195d60
Andy Blackledge <andyblackledge@hotmail.com>
2021-02-27 15:47:14 +0000

    Test test

--- test/MyClass.test.ts
+++ test/MyClass.test.ts
@@ -5,4 +5,5 @@ it('tests something', async () => {
   const myClass = new MyClass(616);
   const result = myClass.perform(Operation.Add, 50);
   expect(result).to.equal(666);
+  expect(result).to.equal(0);
 });
