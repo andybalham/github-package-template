c14bb495e439ed1eab776c3e54b594f9f686be9d
Andy Blackledge <andyblackledge@hotmail.com>
2021-02-27 17:06:20 +0000

    Test coverage

--- test/MyClass.test.ts
+++ test/MyClass.test.ts
@@ -5,5 +5,4 @@ it('tests something', async () => {
   const myClass = new MyClass(616);
   const result = myClass.perform(Operation.Add, 50);
   expect(result).to.equal(666);
-  expect(result).to.equal(0);
 });
